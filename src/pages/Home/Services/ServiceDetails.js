2f9cb743cd1a9281efbad35b4601c111c842032e
moshiur01 <mr.moshiur314@gmail.com>
2021-10-20 22:41:13 +0600

    private route fixed

--- src/pages/Home/Services/ServiceDetails.js
+++ src/pages/Home/Services/ServiceDetails.js
@@ -1,36 +1,26 @@
 import { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import useServices from "../../../hooks/useServices";
-import "./serviceDetails.css";
-// import useServices from "../../../hooks/useServices";
 
 const ServiceDetails = () => {
   // get dynamic route id
   let { serviceId } = useParams();
   console.log(serviceId);
   // get services data
-  const [ser, setSer] = useState();
+  const [ser, setSer] = useState([]);
 
   useEffect(() => {
     fetch("/services.json")
       .then((res) => res.json())
-      .then((data) => setSer(data));
-  }, []);
-
-  // // // load side effects
-  // useEffect(() => {
-  //   const foundService = ser.find(
-  //     (singleService) => singleService.index === serviceId
-  //   );
-  //   console.log(foundService);
-  // }, []);
+      .then((data) => setSer(data[serviceId]));
+  }, [serviceId]);
 
   return (
-    <Container className="serviceDetails">
-      <div>
-        <h1>Can't Get the Idea {serviceId}</h1>
-      </div>
+    <Container className="my-5  text-center shadow p-3 mb-5 bg-body rounded">
+      <img className="rounded my-2" src={ser.image} alt="" />
+      <h5 className="text-primary my-2">{ser.title}</h5>
+      <p>{ser.details}</p>
+      <Button variant="success">Book Now</Button>
     </Container>
   );
 };
